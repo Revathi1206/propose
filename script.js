82c27e1a0bcc5787f783640adb7df9cacb4a6722
Revathi1206 <126352489+Revathi1206@users.noreply.github.com>
2024-01-24 14:18:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var message = document.querySelector(".message");
+document.querySelector(".message").addEventListener("click", function () {
+  document.querySelector(".sym").classList.add("imgad");
+  document.querySelector(".text1").classList.add("cladd");
+  document.querySelector(".text2").classList.add("cladd2");
+  document.querySelector(".text3").classList.add("cladd3");
+  document.querySelector(".text5").classList.add("cladd");
+  document.querySelector(".text6").classList.add("cladd2");
+  document.querySelector(".text7").classList.add("cladd3");
+  document.querySelector(".text8").classList.add("cladd");
+  document.querySelector(".text9").classList.add("cladd2");
+  document.querySelector(".text4").classList.add("cladd3");
+  document.querySelector(".text10").classList.add("cladd");
+
+  message.style.display = "none";
+});
